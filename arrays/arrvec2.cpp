9be623dbfd64b3c88f9951fccacf56c4a787b761
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-10-25 16:07:37 +0530

    vector code

--- arrays/arrvec2.cpp
+++ arrays/arrvec2.cpp
@@ -12,6 +12,9 @@ int main(){
     vec.push_back(5);
     vec.push_back(6);
     vec.push_back(7);
+    vec.push_back(8);
+    vec.push_back(9);
+    vec.push_back(10);
   cout << vec.size() << endl;
   cout << vec.capacity() << endl; 
     return 0;
