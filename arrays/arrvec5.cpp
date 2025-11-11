fcb275cfe0a73913dd4c17b6a800b7182fa69599
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-11-11 19:26:55 +0530

    vector program

--- arrays/arrvec5.cpp
+++ arrays/arrvec5.cpp
@@ -9,6 +9,11 @@ int main() {
   	v.push_back('x');
   	v.push_back('y');
   	v.push_back('z');
+  	v.push_back('p');
+  	v.push_back('q');
+  	v.push_back('r');
+  	v.push_back('s');
+  	v.push_back('t');
   
   	// Inserting 'c' at index 1
   	v.insert(v.begin() + 1, 'c');
