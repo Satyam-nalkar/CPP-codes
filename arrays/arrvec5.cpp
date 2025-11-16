5878ee7844c75a7097f5e9426b076e281bc45583
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-11-16 11:05:13 +0530

    vector program 5

--- arrays/arrvec5.cpp
+++ arrays/arrvec5.cpp
@@ -6,6 +6,10 @@ int main() {
     vector<char> v = {'a','b', 'f', 'd'};
   
   	// Inserting 'z' at the back
+  	v.push_back('f');
+  	v.push_back('g');
+  	v.push_back('h');
+  	v.push_back('i');
   	v.push_back('j');
   	v.push_back('k');
   	v.push_back('l');
