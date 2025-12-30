9462918025d606552b5211708a22249538f55ef6
Satyam Shankar Nalkar <satyamnalkar95@gmail.com>
2025-12-30 23:44:04 +0530

    element 43

---
+++ arrvec4.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40,50,60,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37};
+    
+    // Accessing using operator[]
+    cout << "Element at index 2 using []: " << v[2] << endl;
+    cout << "Element at index 3 using []: " << v[3] << endl;
+    cout << "Element at index 1 using []: " << v[1] << endl;
+    cout << "Element at index 4 using []: " << v[4] << endl;
+    cout << "Element at index 6 using []: " << v[6] << endl;
+    cout << "Element at index 7 using []: " << v[7] << endl;
+    cout << "Element at index 8 using []: " << v[8] << endl;
+    cout << "Element at index 9 using []: " << v[9] << endl;
+    cout << "Element at index 10 using []: " << v[10] << endl;
+    cout << "Element at index 11 using []: " << v[11] << endl;
+    cout << "Element at index 12 using []: " << v[12] << endl;
+    cout << "Element at index 13 using []: " << v[13] << endl;
+    cout << "Element at index 14 using []: " << v[14] << endl;
+    cout << "Element at index 15 using []: " << v[15] << endl;
+    cout << "Element at index 16 using []: " << v[16] << endl;
+    cout << "Element at index 17 using []: " << v[17] << endl;
+    cout << "Element at index 18 using []: " << v[18] << endl;
+    cout << "Element at index 22 using []: " << v[22] << endl;
+    cout << "Element at index 19 using []: " << v[19] << endl;
+    cout << "Element at index 20 using []: " << v[20] << endl;
+    cout << "Element at index 21 using []: " << v[21] << endl;
+    cout << "Element at index 22 using []: " << v[22] << endl;
+    cout << "Element at index 23 using []: " << v[23] << endl;
+    cout << "Element at index 24 using []: " << v[24] << endl;
+    cout << "Element at index 25 using []: " << v[25] << endl;
+    cout << "Element at index 26 using []: " << v[26] << endl;
+    cout << "Element at index 27 using []: " << v[27] << endl;
+    cout << "Element at index 28 using []: " << v[28] << endl;
+    cout << "Element at index 29 using []: " << v[29] << endl;
+    cout << "Element at index 30 using []: " << v[30] << endl;
+    cout << "Element at index 31 using []: " << v[31] << endl;
+    cout << "Element at index 32 using []: " << v[32] << endl;
+    cout << "Element at index 33 using []: " << v[33] << endl;
+    cout << "Element at index 34 using []: " << v[34] << endl;
+    cout << "Element at index 35 using []: " << v[35] << endl;
+    cout << "Element at index 36 using []: " << v[36] << endl;
+    cout << "Element at index 37 using []: " << v[37] << endl;
+    cout << "Element at index 38 using []: " << v[38] << endl;
+    cout << "Element at index 39 using []: " << v[39] << endl;
+    cout << "Element at index 40 using []: " << v[40] << endl;
+    cout << "Element at index 41 using []: " << v[41] << endl;
+    cout << "Element at index 42 using []: " << v[42] << endl;
+    cout << "Element at index 43 using []: " << v[43] << endl;
+    // Accessing using at()
+    cout << "Element at index 3 using at(): " << v.at(3) << endl;
+    cout << "Element at index 1 using at(): " << v.at(1) << endl;
+    cout << "Element at index 2 using at(): " << v.at(2) << endl;
+    cout << "Element at index 4 using at(): " << v.at(4) << endl;
+    cout << "Element at index 5 using at(): " << v.at(5) << endl;
+    cout << "Element at index 6 using at(): " << v.at(6) << endl;
+    cout << "Element at index 7 using at(): " << v.at(7) << endl;
+    cout << "Element at index 8 using at(): " << v.at(8) << endl;
+    cout << "Element at index 9 using at(): " << v.at(9) << endl;
+    
+    return 0;
+}
